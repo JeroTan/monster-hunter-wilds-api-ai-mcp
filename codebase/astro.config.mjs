0aa57914b1f4ae71c3c8f5868cdf8ae589c14dc0
Tan Jerowe <86645548+JeroTan@users.noreply.github.com>
2025-10-10 18:46:53 +0800

    feat: update codebase

---
+++ codebase/astro.config.mjs
@@ -0,0 +1,70 @@
+// @ts-check
+import { defineConfig, envField } from 'astro/config';
+
+import cloudflare from '@astrojs/cloudflare';
+import tailwindcss from '@tailwindcss/vite';
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  adapter: cloudflare({
+    imageService: "compile"
+  }),
+  image: {
+    // Allow processing all images from remote. This allow modifying the images size depending on the device.
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.placehold.co",
+      },
+      {
+        protocol: "https",
+        hostname: "**.imagedelivery.net",
+      },
+    ],
+  },
+  devToolbar: {
+    enabled: false
+  },
+  output: 'server',
+  prefetch: {
+    defaultStrategy: 'viewport'
+  },
+   env: {
+    schema: {
+      ENVIRONMENT_STATUS: envField.string({ context: "server", access: "public", default: "live" }),
+    },
+  },
+  vite: {
+    ssr: {
+      external: ['node:buffer'],
+    },
+    plugins: [
+      tailwindcss()
+    ],
+  },
+  server: {
+    port: 3001,
+    host: true
+  },
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "ph"],
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
+  integrations: [sitemap({
+    filter: (page) =>
+      page !== "/500" &&
+      page !== "/404",
+    i18n: {
+      defaultLocale: 'en',
+      locales: {
+        en: 'en-PH',
+        ph: 'ph-PH',
+      }
+    }
+  })],
+  site:"https://__YOUR_DOMAIN__.com/",
+});
